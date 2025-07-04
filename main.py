46a4f93f0d8cc1d4c27e16a4c95a3b263d60195a
dcook <daniel.j.cook.28@gmail.com>
2025-07-04 14:59:00 -0500

    added display of proper units of measure when presenting average speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import (QGridLayout, QLineEdit, QLabel, QApplication, QPushButton, QWidget,
-                             QListWidget)
+                             QComboBox)
 import sys
 
 class SpeedCalculator(QWidget):
@@ -11,17 +11,21 @@ class SpeedCalculator(QWidget):
         distance_label = QLabel("Distance: ")
         self.distance_line_edit = QLineEdit()
 
-        time_label = QLabel("Time: ")
+        time_label = QLabel("Time (hours): ")
         self.time_line_edit = QLineEdit()
 
         calculate_button = QPushButton("Convert")
         calculate_button.clicked.connect(self.calculate)
+
+        self.metric_combo = QComboBox()
+        self.metric_combo.addItems(["Metric (km)", "Imperial (mi)"])
 
         self.output_label = QLabel("")
 
         grid = QGridLayout()
         grid.addWidget(distance_label, 0, 0)
         grid.addWidget(self.distance_line_edit, 0, 1)
+        grid.addWidget(self.metric_combo, 0, 2)
         grid.addWidget(time_label, 1, 0)
         grid.addWidget(self.time_line_edit, 1, 1)
         grid.addWidget(calculate_button, 2, 1)
@@ -38,8 +42,11 @@ class SpeedCalculator(QWidget):
         try:
             distance = float(distance)
             time = float(time)
-            speed = distance/time
-            self.output_label.setText(f"Your speed was {speed}")
+            avg_speed = distance/time
+            if(self.metric_combo.currentText().startswith("Metric")):
+               self.output_label.setText(f"Your average speed was {avg_speed} km/hr")
+            else:
+                self.output_label.setText(f"Your average speed was {avg_speed} mi/hr")
         except:
             self.output_label.setText("Entries must be numbers")
 
